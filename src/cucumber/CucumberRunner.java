195a61c1e11a5eb94787cf3e239841d08d8bc21b
teja_ <teja_@DESKTOP-EO9JQ7U.lan>
2018-02-11 16:41:35 +0100

    Cucumber Test framework Non Maven

---
+++ src/cucumber/CucumberRunner.java
@@ -0,0 +1,13 @@
+package cucumber;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+		format = {"pretty","json:target/cucumber.json"},
+		features = {"src/cucumber/"}
+		)
+public class CucumberRunner {
+
+}
